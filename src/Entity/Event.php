d0f969b2450d94ff0854e4e51f7b54d380a4a6b7
leith101 <leithjendoubi@gmail.com>
2023-11-14 20:13:19 +0100

    Update Event.php

--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -2,66 +2,167 @@
 
 namespace App\Entity;
 
+use App\Repository\EventRepository;
+use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
 
-/**
- * Event
- *
- * @ORM\Table(name="event")
- * @ORM\Entity
- */
+
+#[ORM\Entity(repositoryClass: EventRepository::class)]
+#[ORM\Table(name: "Event")]
 class Event
 {
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="id", type="integer", nullable=false)
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
-     */
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
     private $id;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="nom", type="text", length=65535, nullable=false)
-     */
-    private $nom;
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message:"name is required")]
+    private $name;
 
-    /**
-     * @var float
-     *
-     * @ORM\Column(name="price", type="float", precision=10, scale=0, nullable=false)
+   
+
+     /**
+     * @var int The hashed price
      */
+    #[ORM\Column]
+    #[Assert\NotBlank(message:"price is required")]
     private $price;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="date", type="text", length=65535, nullable=false)
-     */
+    #[ORM\Column(length: 180, unique: true)]
+    #[Assert\NotBlank(message:"date is required")]
     private $date;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="description", type="text", length=65535, nullable=false)
-     */
-    private $description;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="image", type="text", length=65535, nullable=false)
-     */
+    #[ORM\Column(type: "string", length: 255, nullable: true)]
     private $image;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="place", type="text", length=65535, nullable=false)
-     */
+    
+
+    #[ORM\Column(type: "string", length: 255, nullable: true)]
+    #[Assert\NotBlank(message:"place is required")]
     private $place;
+
+    #[ORM\Column(type: "string", length: 255, nullable: true)]
+    #[Assert\NotBlank(message:"description is required")]
+    private $description;
+
+    
+
+     
+   
+ 
+
+
+
+
+     /**
+
+ */
+   /**
+
+ */
+public function getSalt(): ?string
+    {
+        return null;
+    }
+
+/**
+    
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainprice = null;
+    }
+    
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+   
+
+    public function getname(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setname(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getprice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setprice(string $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getdate(): ?string
+    {
+        return $this->date;
+    }
+
+    public function setdate(string $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getplace(): ?string
+    {
+        return $this->place;
+    }
+
+    public function setplace(string $place): static
+    {
+        $this->place = $place;
+
+        return $this;
+    }
+
+
+    public function getdescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setdescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+    public function getResetToken(): ?string
+    {
+        return $this->reset_token;
+    }
+
+    
+
 
 
 }
