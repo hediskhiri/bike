e10c03d5f5d3901d337b3394053b559e0e86a4f4
hediskhiri <hedi.skhiri@hotmail.com>
2023-11-14 19:23:58 +0000

    Merge branch 'main' of https://github.com/hediskhiri/bike

--- src/Entity/Station.php
+++ src/Entity/Station.php
@@ -2,38 +2,69 @@
 
 namespace App\Entity;
 
+use App\Repository\StationRepository;
+use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
+use App\Entity\Panier;
 
-/**
- * Station
- *
- * @ORM\Table(name="station")
- * @ORM\Entity
- */
-class Station
+#[ORM\Entity(repositoryClass: StationRepository::class)]
+#[ORM\Table(name: "Station")]
+class Station 
 {
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="id_s", type="integer", nullable=false)
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
-     */
-    private $idS;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="nom_s", type="string", length=255, nullable=false)
-     */
-    private $nomS;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private $id_s;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="emplacement_s", type="string", length=255, nullable=false)
-     */
-    private $emplacementS;
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message:"name is required")]
+    private $nom_s;
+
+   
+    #[ORM\Column]
+    #[Assert\NotBlank(message:"emplacement is required")]
+    private $emplacement_s;
+    
+
+    public function getId_s(): ?int
+    {
+        return $this->id_s;
+    }
+
+    public function setId_s(int $id_s): static
+    {
+        $this->id_s = $id_s;
+
+        return $this;
+    }
+   
+
+    public function getNom_s(): ?string
+    {
+        return $this->nom_s;
+    }
+
+    public function setNom_s(string $nom_s): static
+    {
+        $this->nom_s = $nom_s;
+
+        return $this;
+    }
+
+    public function getemplacement_s(): ?string
+    {
+        return $this->emplacement_s;
+    }
+
+    public function setemplacement_s(string $emplacement_s): static
+    {
+        $this->emplacement_s = $emplacement_s;
+
+        return $this;
+    }
 
 
+
+    
 }
