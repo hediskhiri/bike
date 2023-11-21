9a02af186c8e56075ab35d40556de33c46b231fa
hediskhiri <hedi.skhiri@hotmail.com>
2023-11-21 22:10:12 +0000

    panier

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -9,77 +9,94 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use App\Repository\ProduitRepository;
 #[Route('/panier')]
 class PanierController extends AbstractController
 {
-    #[Route('/', name: 'app_panier_index', methods: ['GET'])]
-    public function index(EntityManagerInterface $entityManager): Response
-    {
-        $paniers = $entityManager
-            ->getRepository(Panier::class)
-            ->findAll();
+    private $session;
 
-        return $this->render('panier/index.html.twig', [
-            'paniers' => $paniers,
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
+
+
+
+
+
+    ////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////
+    #[Route('/panier/add/{idprod}', name: 'app_panier_add')]
+    public function addToCart($idprod, ProduitRepository $repo)
+    {
+
+        $product = $repo->find($idprod);
+        $cart = $this->session->get('cart', []);
+
+        if (!isset($cart[$idprod])) {
+            $cart[$idprod] = [
+                'id' => $idprod,
+                'name' => $product->getNomprod(),
+                'price' => $product->getPrixprod(),
+                'image' => $product->getImageprod(),
+                'description' => $product->getDescriptionprod(),
+                'quantity' => 0,
+                'total' => 0
+            ];
+        }
+        if (isset($_POST["quantite"])) {
+            $quantite = intval($_POST["quantite"]);
+            if ($quantite == 0)
+                $quantite = 1;
+        } else
+            $quantite = 1;
+
+        $cart[$idprod]['quantity'] += $quantite;
+        $cart[$idprod]['total'] = $quantite * $product->getPrixprod();
+
+        $this->session->set('cart', $cart);
+
+        return $this->redirectToRoute('app_panier_index');
+    }
+    ////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////
+
+    #[Route('/', name: 'app_panier_index')]
+    public function showCart()
+    {
+        $panier = $this->session->get('cart', []);
+        $numberOfItems = count($panier);
+        return $this->render('panier/panier.html.twig', [
+            'paniers' => $panier,
+            'numberOfItems' => $numberOfItems,
         ]);
     }
 
-    #[Route('/new', name: 'app_panier_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/delete/{id}', name: 'app_panier_delete')]
+    public function removeFromCart($id)
     {
-        $panier = new Panier();
-        $form = $this->createForm(PanierType::class, $panier);
-        $form->handleRequest($request);
+        $cart = $this->session->get('cart', []);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            
-            $entityManager->persist($panier);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_panier_index', [], Response::HTTP_SEE_OTHER);
+        if (isset($cart[$id])) {
+            unset($cart[$id]);
         }
 
-        return $this->renderForm('panier/new.html.twig', [
-           
-            'form' => $form,
-        ]);
+        $this->session->set('cart', $cart);
+
+        return $this->redirectToRoute('app_panier_index');
     }
-
-    #[Route('/{idpanier}', name: 'app_panier_show', methods: ['GET'])]
-    public function show(Panier $panier): Response
+    #[Route('/clear-cart', name: 'clear_cart')]
+    public function clearCart(SessionInterface $session): \Symfony\Component\HttpFoundation\RedirectResponse
     {
-        return $this->render('panier/show.html.twig', [
-            'panier' => $panier,
-        ]);
-    }
+        // Clear the cart session
+        $session->remove('cart');
 
-    #[Route('/{idpanier}/edit', name: 'app_panier_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Panier $panier, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(PanierType::class, $panier);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_panier_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('panier/edit.html.twig', [
-            'panier' => $panier,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{idpanier}', name: 'app_panier_delete', methods: ['POST'])]
-    public function delete(Request $request, Panier $panier, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$panier->getIdpanier(), $request->request->get('_token'))) {
-            $entityManager->remove($panier);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_panier_index', [], Response::HTTP_SEE_OTHER);
+        // Redirect to the app_produit_front route
+        
+        return $this->redirectToRoute('app_produit_front');
     }
 }
