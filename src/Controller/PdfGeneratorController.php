19430f372070a8b7bd189d80be491d45719aeea4
hediskhiri <hedi.skhiri@hotmail.com>
2023-11-25 01:33:52 +0000

    api pdf

--- src/Controller/PdfGeneratorController.php
+++ src/Controller/PdfGeneratorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,31 +10,41 @@ use Dompdf\Dompdf;
 
 class PdfGeneratorController extends AbstractController
 {
+    private $session;
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
     #[Route('/pdf/generator', name: 'app_pdf_generator')]
     public function index(): Response
-    { $data = [
-        /*'imageSrc'  => $this->imageToBase64($this->getParameter('kernel.project_dir') . '/public/img/profile.png'),*/
-        'name'         => 'John Doe',
-        'address'      => 'USA',
-        'mobileNumber' => '000000000',
-        'email'        => 'john.doe@email.com'
-    ];
-    $html =  $this->renderView('pdf_generator/index.html.twig', $data);
-    $dompdf = new Dompdf();
-    $dompdf->loadHtml($html);
-    $dompdf->render();
-    return new Response (
-        $dompdf->stream('resume', ["Attachment" => false]),
-        Response::HTTP_OK,
-        ['Content-Type' => 'application/pdf']
-    );
-}
+    {
+        // Récupérer les données du panier depuis la session
+        $panier = $this->session->get('cart', []);
 
-/*private function imageToBase64($path) {
-    $path = $path;
-    $type = pathinfo($path, PATHINFO_EXTENSION);
-    $data = file_get_contents($path);
-    $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
-    return $base64;
-}*/
+        // Données à passer au template Twig
+        $data = [
+          /*  'name' => 'John Doe',
+            'address' => 'USA',
+            'mobileNumber' => '000000000',
+            'email' => 'john.doe@email.com',*/
+            'panier' => $panier,
+        ];
+
+        // Rendre la vue avec les données
+        $html = $this->renderView('pdf_generator/index.html.twig',$data);
+
+        // Générer le PDF
+        $dompdf = new Dompdf();
+        $dompdf->loadHtml($html);
+        $dompdf->render();
+
+        // Retourner la réponse avec le PDF généré
+        return new Response(
+            $dompdf->stream('facture.pdf', ["Attachment" => false]),
+            Response::HTTP_OK,
+            ['Content-Type' => 'application/pdf']
+        );
+    }
 }
