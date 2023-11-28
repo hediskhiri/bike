1d1ce10f97a68c185f1f0605a139c1ae6dbcdb05
Rawef <115536370+Rawef@users.noreply.github.com>
2023-11-28 13:49:54 +0100

    Add files via upload

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -2,83 +2,228 @@
 
 namespace App\Entity;
 
+use App\Repository\UtilisateurRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
-/**
- * Utilisateur
- *
- * @ORM\Table(name="utilisateur", indexes={@ORM\Index(name="idPanier", columns={"idPanier"})})
- * @ORM\Entity
- */
-class Utilisateur
+#[ORM\Entity(repositoryClass: UtilisateurRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
 {
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="username", type="string", length=20, nullable=false)
-     */
-    private $username;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(name: 'id')]
+    private ?int $idUser = null;
+
+    #[ORM\Column(length: 180, unique: true)]
+    private ?string $email = null;
+
+    #[ORM\Column]
+    private array $role = [];
 
     /**
-     * @var string
-     *
-     * @ORM\Column(name="password", type="string", length=100, nullable=false)
+     * @var string The hashed password
      */
-    private $password;
+    #[ORM\Column]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $username = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $gender = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $date = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $reset_token = null;
+
+    #[ORM\Column(length: 255)]
+    private ?int $idPanier = null;
+
+    #[ORM\Column(type: 'boolean')]
+    private $isVerified = false;
+
+
+
+    public function getId(): ?string
+    {
+        //return $this->idUser;
+        return (string) $this->idUser;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+ 
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
 
     /**
-     * @var string
+     * A visual identifier that represents this user.
      *
-     * @ORM\Column(name="email", type="string", length=100, nullable=false)
+     * @see UserInterface
      */
-    private $email;
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
 
     /**
-     * @var string
-     *
-     * @ORM\Column(name="image", type="string", length=100, nullable=false)
+     * @deprecated since Symfony 5.3, use getUserIdentifier instead
      */
-    private $image;
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+   /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+{
+    // Ajoutez le rôle 'client' par défaut s'il n'est pas déjà présent
+    $role = in_array('client', $this->role) ? $this->role : array_merge($this->role, ['client']);
+
+    return array_unique($role);
+}
+
+    public function setRoles(array $roles): static
+    {
+        $this->role = $roles;
+
+        return $this;
+    }
 
     /**
-     * @var int
-     *
-     * @ORM\Column(name="idUser", type="integer", nullable=false)
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @see PasswordAuthenticatedUserInterface
      */
-    private $iduser;
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
 
     /**
-     * @var string|null
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
      *
-     * @ORM\Column(name="gender", type="string", length=100, nullable=true)
+     * @see UserInterface
      */
-    private $gender;
+    public function getSalt(): ?string
+    {
+        return null;
+    }
 
     /**
-     * @var string|null
-     *
-     * @ORM\Column(name="date", type="string", length=100, nullable=true)
+     * @see UserInterface
      */
-    private $date;
-
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="Role", type="string", length=100, nullable=true)
-     */
-    private $role;
-
-    /**
-     * @var \Panier
-     *
-     * @ORM\ManyToOne(targetEntity="Panier")
-     * @ORM\JoinColumns({
-     *   @ORM\JoinColumn(name="idPanier", referencedColumnName="idPanier")
-     * })
-     */
-    private $idpanier;
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
 
 
+    public function getUsernamee(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+     public function getGender(): ?string
+    {
+        return $this->gender;
+    }
+
+    public function setGender(?string $gender): static
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    public function getResetToken(): ?string
+    {
+        return $this->reset_token;
+    }
+
+    public function setResetToken(?string $reset_token): self
+    {
+        $this->reset_token = $reset_token;
+
+        return $this;
+    }
+
+    public function getDate(): ?string
+    {
+        return $this->date;
+    }
+
+    public function setDate(?string $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getIdpanier(): ?string
+    {
+        return $this->idPanier;
+    }
+
+    public function setIdpanier(?string $idPanier): static
+    {
+        $this->idPanier = $idPanier;
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): static
+    {
+        $this->isVerified = $isVerified;
+
+        return $this;
+    }
 }
